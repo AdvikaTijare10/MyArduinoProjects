bed255ffe7f917cd8021423e0cc159d802d74196
Advika Tijare <155163783+AdvikaTijare10@users.noreply.github.com>
2026-03-14 18:30:13 +0530

    Add files via upload

---
+++ LedProjects/SeriesRightToLeft/seriesRightTOLeft.ino
@@ -0,0 +1,17 @@
+int ledPins[8]={2,3,4,5,9,10,11,12};
+void setup() {
+  // put your setup code here, to run once:
+for(int i=0;i<8;i++)pinMode(ledPins[i],OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for(int i=0;i<8;i++){
+  digitalWrite(ledPins[i],HIGH);
+  delay(100);
+}
+for(int i=7;i>=0;i--){
+  digitalWrite(ledPins[i],LOW);
+  delay(100);
+}
+}
